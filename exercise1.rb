4fc47b56ab3126908eb26a4d0eb4731438b3b4c3
Gilberto Alvarado <galvarado@tacitknowledge.com>
2012-03-05 08:31:15 -0600

    Better looking output

--- exercise1.rb
+++ exercise1.rb
@@ -7,11 +7,15 @@ puts "Provided number is #{n}"
 if n[1] > n[0]
   buffer[1] = n[0]
   buffer[0] = n[1]
-else
-  buffer[2] = n[1]
+elsif n[1] > n[2]
+  
+  buffer[2] = n[0]
   buffer[1] = "0"
-  buffer[0] = n[0]
+  buffer[0] = n[1]
 end
 
-puts "Next number is #{buffer.to_s}"
+puts "Next number is #{buffer.join("")}"
 
+def get_greater(n1, n2)
+  return n1 > n2 ? n1 : n2
+end
