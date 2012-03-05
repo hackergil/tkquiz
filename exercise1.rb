0a99fe0177066a15ec2be5b3e5333b9102f728d7
Gilberto Alvarado <galvarado@tacitknowledge.com>
2012-03-05 09:54:25 -0600

    Solution for 1 and 2 digits, the right way

--- exercise1.rb
+++ exercise1.rb
@@ -1,21 +1,33 @@
 #!/usr/bin/env ruby
 
-n = ARGV[0]
-buffer = []
-puts "Provided number is #{n}"
-
-if n[1] > n[0]
-  buffer[1] = n[0]
-  buffer[0] = n[1]
-elsif n[1] > n[2]
-  
-  buffer[2] = n[0]
-  buffer[1] = "0"
-  buffer[0] = n[1]
-end
-
-puts "Next number is #{buffer.join("")}"
-
 def get_greater(n1, n2)
   return n1 > n2 ? n1 : n2
 end
+
+def calculate_next(number)
+  if number == 1
+    number += 9
+  else
+    number += 10
+  end
+  number
+end
+
+n = ARGV[0]
+puts "Provided number is #{n}"
+
+if n.length == 1
+  puts "Next number is #{calculate_next(n.to_i)}"
+else
+  n = n.scan(/./)
+  if n[1] > n[0]
+    n = n.reverse
+  else
+    a = n
+    a.insert(1,"0")
+  end
+  puts "Next number is #{n}"
+end
+
+
+
