59f8b3461f7f8879fc060512995cf9d7ed0c53be
Gilberto Alvarado <galvarado@tacitknowledge.com>
2012-03-05 18:26:03 -0600

    RTFRT

--- exercise1.rb
+++ exercise1.rb
@@ -1,33 +1,26 @@
 #!/usr/bin/env ruby
 
+# EXERCISE 1
+# Given a number, find the next higher number that uses the same set of digits. 
+# For instance, given the number 38276, the next higher number that uses the digits 2 3 6 7 8 is 38627.
+
 def get_greater(n1, n2)
   return n1 > n2 ? n1 : n2
-end
-
-def calculate_next(number)
-  if number == 1
-    number += 9
-  else
-    number += 10
-  end
-  number
 end
 
 n = ARGV[0]
 puts "Provided number is #{n}"
 
-if n.length == 1
-  puts "Next number is #{calculate_next(n.to_i)}"
+if n.length <= 1
+  puts "Next number is #{n.to_i}"
 else
   n = n.scan(/./)
   if n[1] > n[0]
     n = n.reverse
-  else
-    a = n
-    a.insert(1,"0")
   end
-  puts "Next number is #{n}"
+  puts "Next number is #{n.join("")}"
 end
+
 
 
 
