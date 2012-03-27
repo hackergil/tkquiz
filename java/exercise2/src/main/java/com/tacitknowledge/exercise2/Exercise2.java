28b1099e4b1750b39e46df04fe3ab674cf67c0dc
Gilberto Alvarado <galvarado@tacitknowledge.com>
2012-03-27 08:48:32 -0600

    Fulfilling exercise 2

--- java/exercise2/src/main/java/com/tacitknowledge/exercise2/Exercise2.java
+++ java/exercise2/src/main/java/com/tacitknowledge/exercise2/Exercise2.java
@@ -1,5 +1,6 @@
 package main.java.com.tacitknowledge.exercise2;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,7 +14,25 @@ public class Exercise2
 {
 
     public List<Integer> getNumbersAsSumOfTargetNumber(List<Integer> numbersList, Integer targetNumber) {
-        //TODO Implement
-        return null;
+        List<Integer> resultList = null;
+        if(numbersList != null && numbersList.size() > 2)
+        {
+            for (int i=0; i<numbersList.size() - 1; i++)
+            {
+                for(int j = i + 1; j< numbersList.size(); j++)
+                {
+                    Integer n1 = numbersList.get(i);
+                    Integer n2 = numbersList.get(j);
+                    if (n1 + n2 == targetNumber)
+                    {
+                        resultList = new ArrayList<Integer>();
+                        resultList.add(n1);
+                        resultList.add(n2);
+                        return resultList;
+                    }
+                }
+            }
+        }
+        return resultList;
     }
 }
--- java/exercise2/src/test/java/Exercise2Test.java
+++ java/exercise2/src/test/java/Exercise2Test.java
@@ -5,8 +5,13 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
 import static junit.framework.Assert.assertNull;
+import static junit.framework.Assert.assertTrue;
 
 /**
  * @author Gilberto Alvarado(galvarado@tacitknowledge.com)
@@ -22,16 +27,25 @@ public class Exercise2Test
         exercise = new Exercise2();
     }
 
-
     @Test
     public void shouldFindTwoNumbersOnArray()
     {
-
+        Integer[] numbers = {1, 2, 3, 4, 5, 6};
+        Integer target = 9;
+        List<Integer> result = exercise.getNumbersAsSumOfTargetNumber(Arrays.asList(numbers), target);
+        assertNotNull(result);
+        assertEquals(2, result.size());
+        assertTrue(result.get(0) + result.get(1) == target);
     }
 
     @Test
     public void shouldReturnNull()
     {
-        assertNull(exercise.getNumbersAsSumOfTargetNumber(new ArrayList<Integer>(), 1));
+        List<Integer> numbers = new ArrayList<Integer>();
+        assertNull(exercise.getNumbersAsSumOfTargetNumber(numbers, 1));
+        numbers.add(0);
+        assertNull(exercise.getNumbersAsSumOfTargetNumber(numbers, 1));
+        numbers.add(0);
+        assertNull(exercise.getNumbersAsSumOfTargetNumber(numbers, 1));
     }
 }
